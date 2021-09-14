70b7255f68db10173a11a6726e8ec2ef7c8eebf5
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-09-14 11:28:08 -0400

    carousel rt and lf buttons, one card displays at a time

--- portfolio/src/components/Card.js
+++ portfolio/src/components/Card.js
@@ -3,9 +3,9 @@ import React from 'react';
 const {PUBLIC_URL} = process.env;
 console.log(PUBLIC_URL);
 
-const Card = ({ name, img }) => {
+const Card = ({ name, img, cardStyle }) => {
     return (
-        <div className="card">
+        <div className={`card ${cardStyle}`}>
             <img src={PUBLIC_URL + img} alt={name} />
             <h3>{name}</h3>
         </div>
--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -4,14 +4,55 @@ import '../styles/Work.css';
 import Card from './Card';
 import { data } from './data';
 
-const Work = () => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
+import { useState } from 'react';
+
+
+const Work = () => {    
     
+    const [index, setIndex] = useState(0);
+
+    const slideLeft = () => {
+        if (index -1 >= 0) {
+            setIndex(index - 1);
+        }
+    };
+
+    const slideRight = () => {
+        if (index + 1 <= data.length - 1) {
+            setIndex(index + 1);
+        }
+    };
+
+
+
+
     return (
         <div className="Work">
             <div className="container">
-                {data.map((project) => {
-                    return <Card key={project.id} {...project} />;
+                <div className="card-container">
+
+                    <FontAwesomeIcon
+                        onClick={slideLeft}
+                        className="leftBtn"
+                        icon={faChevronLeft}
+                    />
+
+                    <FontAwesomeIcon
+                        onClick={slideRight}
+                        className="rightBtn"
+                        icon={faChevronRight}
+                    />
+
+                {data.map((project, n) => {
+                    let position = n > index ? "nextCard" 
+                        : n === index ? "activeCard" 
+                        : "prevCard";
+                    return <Card key={project.id} {...project} cardStyle={position} />;
                 })}
+                </div>
             </div>
         </div>
     );
