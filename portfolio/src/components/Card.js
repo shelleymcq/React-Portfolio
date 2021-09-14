a81d5e5aafc119cc26e885367943d1fd732a2305
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-09-14 14:55:53 -0400

    conditional rendering of deployed link in card

--- portfolio/src/components/Card.js
+++ portfolio/src/components/Card.js
@@ -3,11 +3,19 @@ import React from 'react';
 const {PUBLIC_URL} = process.env;
 console.log(PUBLIC_URL);
 
-const Card = ({ name, img, cardStyle }) => {
+const Card = ({ name, img, repo, deployed, link, description, tech, cardStyle }) => {
     return (
         <div className={`card ${cardStyle}`}>
             <img src={PUBLIC_URL + img} alt={name} />
-            <h3>{name}</h3>
+            <div>
+                <h3>{name}</h3>
+                <p>{description}</p>
+                <p>{tech}</p>
+            </div>  
+            <div className={'links'}>
+                <a href={repo} target="_blank" rel="noopener noreferrer">The Repo</a>
+                {deployed && <a href={link} target="_blank" rel="noopener noreferrer">The App</a>}
+            </div>
         </div>
     );
 };
