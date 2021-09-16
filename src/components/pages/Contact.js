50e5e2ba0f0808bc0a751d687af546631cf56eaa
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-09-16 17:01:39 -0400

    downloadable resume

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import '../../styles/Contact.css';
+import Pdf from '../../resume.pdf';
 
 const Contact = () => {
     return (
         <section className="contact">
-            <h2>Find Me Online</h2>
+            <h2>Resume</h2>
+            <a href={Pdf} target ="_blank">download my resume</a>
+            <p>
+            Solution-oriented Full Stack Web Developer applying teaching background and strong technical skills towards building client-side and server-side applications. Earned my certificate in web development from Georgia Tech, where I enhanced my skills in HTML, CSS, JavaScript, node.js, MySQL, MongoDB, and React. My science teaching experience has honed my ability to communicate, collaborate, and manage learners of all levels. I want a role that leverages both my tech and teaching skills to create applications and experiences for learners and information seekers who want to make the world, and web, a more beautiful and accessible place to learn.
+            </p>
+
+            <h3>Front-End Proficiencies</h3>
             <ul>
-                <li><a href="mailto:shelleymcq.dev@gmail.com">shelleymcq.dev@gmail.com</a></li>                 
-                <li><a href="https://github.com/shelleymcq" target="_blank" rel="noopener noreferrer">Github</a></li>
-                <li><a href="https://www.linkedin.com/in/shelleymchardy/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+                <li>Bulma</li>
+                <li>Responsive Design</li>
+            </ul>
+            <h3>Back-End Proficiencies</h3>
+            <ul>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>MySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>GraphQL</li>
             </ul>
         </section>
     )
